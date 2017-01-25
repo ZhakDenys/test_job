d942deae4cc2ef2acf5e57c578ee7e1398e8ec0f
ZhakDenys <napster.sean.parker@gmail.com>
2017-01-26 01:43:53 +0200

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,48 @@
+/*
+ Abstract class for description of Employee
+ */
+import java.io.Serializable;
+
+public abstract class Employee implements Serializable {
+    public static int count;
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary){
+        count++;
+        id = count;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public double averageMonthSalary(){
+        return 0;
+    }
+
+    public String toString() {
+        return getId() + ": Average monthly salary of " +
+                getName() + " equals to " + averageMonthSalary();
+    }
+}
+
