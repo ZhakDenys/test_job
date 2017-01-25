ce003e6160f06de386b1359d29ed5f020968421e
ZhakDenys <napster.sean.parker@gmail.com>
2017-01-26 01:44:35 +0200

    Create EmployeeHourlyWage.java

---
+++ EmployeeHourlyWage.java
@@ -0,0 +1,13 @@
+/*
+ Class for employee with hourly wage
+ */
+
+public class EmployeeHourlyWage extends Employee{
+    public EmployeeHourlyWage(String name, int salary) {
+        super(name, salary);
+    }
+
+    public double averageMonthSalary() {
+        return 20.8 * 8 * getSalary();
+    }
+}
